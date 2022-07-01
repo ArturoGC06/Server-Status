a4e911a3597a5bc0d5717acde3dd3c5c6d78b29e
Ibicado <108338500+Ibicado@users.noreply.github.com>
2022-07-01 13:32:29 +0200

    Add files via upload

--- HTML/login.js
+++ HTML/login.js
@@ -1,9 +1,30 @@
-function comprobarcred (event) {
+/* function comprobarcred (event) {
+	var contrasena = document.getElementById("password").value
+	if (contrasena == "patata") and {
+		window.location.href = "./index.html";
+	}
+	else {
+		window.alert("Contraseña incorrecta")
+		console.log("contraseña incorrecta")
+	}
 	var usuario = document.getElementById("user").value
-	console.log(usuario)
-	if (usuario == "Hola"){
-	console.log("Redirect info.html")
-	window.location.href = "./info.html";
-	
+	if (usuario == "pata"){
+		window.location.href = "./index.html";
+	}
+	else {
+		window.alert("Contraseña usuario incorrecto")
+		console.log("contraseña correcta")
+	}
+} */
+
+function comprobarcred (event) {
+	var contrasena = document.getElementById("password").value
+	var usuario = document.getElementById("user").value
+	if ((contrasena == "patata") && (usuario == "admin")){
+		window.location.href = "./inicio.html";
+	}
+	else {
+		window.alert("El usuario o la contraseña es incorrecta")
+		console.log("contraseña o usuario incorrecta")
 	}
 }
