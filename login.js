d928e6b71bf509daf3efc88fbc13b2d6d12f2b34
ArturoGC06 <76574534+ArturoGC06@users.noreply.github.com>
2022-07-01 14:14:23 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+/* function comprobarcred (event) {
+	var contrasena = document.getElementById("password").value
+	if (contrasena == "patata") and {
+		window.location.href = "./index.html";
+	}
+	else {
+		window.alert("Contraseña incorrecta")
+		console.log("contraseña incorrecta")
+	}
+	var usuario = document.getElementById("user").value
+	if (usuario == "pata"){
+		window.location.href = "./index.html";
+	}
+	else {
+		window.alert("Contraseña usuario incorrecto")
+		console.log("contraseña correcta")
+	}
+} */
+
+function comprobarcred (event) {
+	var contrasena = document.getElementById("password").value
+	var usuario = document.getElementById("user").value
+	if ((contrasena == "patata") && (usuario == "admin")){
+		window.location.href = "./inicio.html";
+	}
+	else {
+		window.alert("El usuario o la contraseña es incorrecta")
+		console.log("contraseña o usuario incorrecta")
+	}
+}
