96ac1810499cf12d5d016125e6a1d9d57e604162
ArturoGC06 <76574534+ArturoGC06@users.noreply.github.com>
2022-07-01 10:33:09 +0200

    Add files via upload

--- HTML/inicio.js
+++ HTML/inicio.js
@@ -3,16 +3,24 @@ function sleep(ms){
 }
 window.onload = async function inicio(event) {
 	console.log('Inicia')
+
+
 	
-	document.getElementById("indexid").classList.remove("index");
+/*	document.getElementById("indexid").classList.remove("index");
 	console.log('Llega')
 	document.getElementById("inicioid").classList.add("iniciobar");
-	console.log('Aqui tb')
+	console.log('Aqui tb') */
+	var ocultar = document.getElementById('principal')
+	ocultar.style.visibility = 'hidden';
+	$( "body" ).removeClass( "claro" );
+	$( "body" ).addClass( "oscuro" );
+
 	
 	for (var i = 0 ; i < 100; i++) {
 		var seconds = 0;
-		console.log(i)
-		await sleep(50);
+		
+		window.scrollTo(0, 0)
+		await sleep(60);
 		var bar = document.getElementById('pgbar').style.width
 		var progreso = parseInt(bar) + 1
 		document.getElementById("pgbar").style.width = String(progreso).concat("","%")
@@ -20,7 +28,11 @@ window.onload = async function inicio(event) {
 		var contadorInt = parseInt(contador) + 1
 		document.getElementById("pgbar").innerHTML = String(contadorInt).concat("","%")
 		}
-	
+	await sleep(140);
+	document.getElementById('welcome').remove()
+	ocultar.style.visibility = 'visible';
+	$( "body" ).removeClass( "oscuro" );
+	$( "body" ).addClass( "claro" );
 	/* const ocultarpcarga = document.getElementsByClassName('iniciobar')[0];
 	ocultar.style.visibility = 'hidden'; 
 
