421c64b0adedfc1f904d1bb3ebd8d52cdee42bfe
ArturoGC06 <agcarbajo1@gmail.com>
2022-07-02 04:22:37 +0200

    Modo oscuro automático

--- info.js
+++ info.js
@@ -1,6 +1,35 @@
+var matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+if(matched){
+      $( "body" ).removeClass( "claro" );
+	  $( "body" ).addClass( "oscuro-auto" );
+	  $( "p" ).removeClass( "claro" );
+	  $( "p" ).addClass( "oscuro" );
+	  $( "ul" ).removeClass( "claro" );
+	  $( "ul" ).addClass( "oscuro" );
+	  $( "label" ).removeClass( "claro" );
+	  $( "label" ).addClass( "oscuro" );
+	  $( "h1" ).removeClass( "claro" );
+	  $( "h1" ).addClass( "oscuro" );
+	console.log('Modo oscuro activado');
+}
+else{
+	      $( "body" ).removeClass( "oscuro" );
+		$( "body" ).addClass( "claro-auto" );
+      $( "p" ).removeClass( "oscuro" );
+		$( "p" ).addClass( "claro" );
+      $( "ul" ).removeClass( "oscuro" );
+		$( "ul" ).addClass( "claro" );
+      $( "label" ).removeClass( "oscuro" );
+		$( "label" ).addClass( "claro" );
+      $( "h1" ).removeClass( "oscuro" );
+		$( "h1" ).addClass( "claro" );
+	console.log('Modo claro activado');
+}
+
 function tema(event){
 	if( $( "body" ).hasClass( "claro" )) {
       $( "body" ).removeClass( "claro" );
+      $( "body" ).removeClass( "claro-auto" );
 	  $( "body" ).addClass( "oscuro" );
 	  $( "p" ).removeClass( "claro" );
 	  $( "p" ).addClass( "oscuro" );
@@ -12,6 +41,7 @@ function tema(event){
 	  $( "h1" ).addClass( "oscuro" );
     } else {
       $( "body" ).removeClass( "oscuro" );
+      $( "body" ).removeClass( "oscuro-auto" );
 		$( "body" ).addClass( "claro" );
       $( "p" ).removeClass( "oscuro" );
 		$( "p" ).addClass( "claro" );
